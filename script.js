db7a3d0e42d07be4f54c3c7f381244db37ec85f3
Tom <tomtoneefx@gmail.com>
2023-07-25 22:44:26 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+    
+    const html = document.documentElement
+    /*
+    if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    } 
+    */
+    html.classList.toggle('light')   
+
+    const img = document.querySelector('#profile img')
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('alt', 'Foto do Myake Brito com oculos escuro')
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('alt', 'Foto do Myake Brito')
+    }
+}
